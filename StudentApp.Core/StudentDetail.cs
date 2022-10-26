74176160cf8e8365b9b12b9371b8a80d75fa6d5c
PasanBistec <pasan@bistecglobal.com>
2022-10-26 18:26:31 +0530

    Add project files.

---
+++ StudentApp.Core/StudentDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentApp.Core
+{
+    public class StudentDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string ContactNo { get; set; }
+
+    }
+}
---
+++ StudentApp.Data/StudentAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentApp.Data
+{
+    public class StudentAppDBContext :DbContext
+    {
+        public StudentAppDBContext(DbContextOptions<StudentAppDBContext>options): base(options)
+        {
+
+        }   
+    }
+}
